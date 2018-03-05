a06c798a561bbb0a9e870d00d138298086545a97
Humberto <humberto@2com.com.mx>
2018-03-05 14:41:00 -0600

    Correccion de caracteres extras

--- views/layouts/classic/topBar/main.php
+++ views/layouts/classic/topBar/main.php
@@ -47,7 +47,7 @@ AppAsset::register($this);
   })(document, window, jQuery);
   </script>
 
-  <?=isset($this->params['modal'])?$this->params['modal']:''?>">
+  <?=isset($this->params['modal'])?$this->params['modal']:''?>
 </body>
 </html>
 <?php $this->endPage() ?>
