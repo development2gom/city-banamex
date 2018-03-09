9a92f23cef578268fdb218a076201375f5f4a71f
Humberto <humberto@2com.com.mx>
2018-03-09 09:35:35 -0600

    Cambio de texto en el footer

--- views/components/classic/topbar/footerBlank.php
+++ views/components/classic/topbar/footerBlank.php
@@ -5,7 +5,7 @@ use yii\helpers\Url;
 ?>
 <div class="login-footer">
       <div class="login-footer-copyright">
-        <p>© Brightstar 2018 </p>
+        <p>© Brightstar <?=date("Y")?> </p>
       </div>
       <div class="login-footer-author">
         <img  src="<?=Url::base()?>/webAssets/images/footer.png" />
