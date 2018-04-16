2b30f710de72a163eee524a326507b52964a94de
Humberto <humberto@2com.com.mx>
2018-04-16 15:47:00 -0500

    autocompletado

--- views/citas/index.php
+++ views/citas/index.php
@@ -375,3 +375,5 @@ $this->registerJsFile(
 </style>
 
    
+   <input style="display:none" type="text" name="fakeusernameremembered"/>
+<input style="display:none" type="password" name="fakepasswordremembered"/>
